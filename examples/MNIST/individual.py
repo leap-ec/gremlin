385a47ebdc43217bb2dc1795e08ad334c2077aa6
Mark Coletti <colettima@ornl.gov>
2022-02-06 21:54:50 -0500

    Individual now inherits from DistributedIndividual to get some additional bookkeeping

--- examples/MNIST/individual.py
+++ examples/MNIST/individual.py
@@ -3,12 +3,15 @@
     DistributedIndividual subclass to allow for using UUIDs to create sub-
     directories to save CARLA state for each individual.
 """
-from leap_ec..individual import RobustIndividual
+from leap_ec.distrib.individual import DistributedIndividual
 
 
-class MNISTIndividual(RobustIndividual):
-    """ We inherit from RobustIndividual so that exceptions are caught and
-        processed such that fitness will be NaNs.
+class MNISTIndividual(DistributedIndividual):
+    """ We inherit from DistributedIndividual so that exceptions are caught and
+        processed such that fitness will be NaNs.  We are also setting up
+        for additional bookkeeping for eval start and stop times that are
+        managed by that class, as well as tracking unique birth IDs when we
+        start using leap_ec.distrib EAs.
     """
     def __init__(self, genome, decoder=None, problem=None):
         super().__init__(genome, decoder, problem)
