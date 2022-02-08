b5f3e4ab32ba46899b6882c6f88639b90cb6b43c
Mark Coletti <colettima@ornl.gov>
2022-02-08 11:14:24 -0500

    Adding start of bespoke probe

---
+++ examples/MNIST/probe.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+''' Define a bespoke LEAP probe for printing individuals to a CSV file.
+'''
+from pathlib import Path
+
+from rich import print
+from rich import pretty
+pretty.install()
+
+from rich.traceback import install
+install()
+
+class IndividualProbeCSV():
+    """
+        Will write out each individual as it receives it in a pipeline and then
+        pass it down the pipeline.
+    """
+    def __init__(self, csv_file):
+        super().__init__()
+        self.csv_file = Path(csv_file)
+
+    def __call__(self, next_individual):
+        """ append the individual to the CSV file
+        """
+        while True:
+            individual = next(next_individual)
+            print(f'{individual!s}')
+
+            yield individual
