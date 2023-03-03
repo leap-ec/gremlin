566d2f1aeeccfe59b2b0aafef7aa05fbc3cb06e9
Mark Coletti <mcoletti@gmail.com>
2023-03-02 21:53:05 -0500

    Consecutively run three different approaches to solving the MNIST problem

--- examples/MNIST/run.sh
+++ examples/MNIST/run.sh
@@ -4,4 +4,13 @@
 #
 
 # Plain vanilla by-generation mode
-../../gremlin/gremlin.py --debug config/common.yml config/bygen.yml
+time ../../gremlin/gremlin.py config/common.yml config/bygen.yml
+
+# Parallel by-generation mode; this actually takes a couple seconds longer
+# than the serial evaluation because of the overhead of Dask setup and teardown.
+# However, Dask works well for problems with significant evaluation time.
+time ../../gremlin/gremlin.py config/common.yml config/distributed.yml config/bygen.yml config/bygen-dist.yml
+
+# This is for running as an asynchronous steady-state EA; the same caveats
+# for the previous run for using Dask applies.
+time ../../gremlin/gremlin.py config/common.yml config/distributed.yml config/async.yml
